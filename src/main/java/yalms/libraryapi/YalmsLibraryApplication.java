8e930811a2750d443782c952a36f277e8a053e61
Shacklz <42877835+Shacklz@users.noreply.github.com>
2018-10-23 21:06:13 +0200

    Add entityscan to get rid of annoying warnings on startup

--- src/main/java/yalms/libraryapi/YalmsLibraryApplication.java
+++ src/main/java/yalms/libraryapi/YalmsLibraryApplication.java
@@ -3,11 +3,13 @@ package yalms.libraryapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableNeo4jRepositories(basePackages = "yalms.libraryapi.repositories")
+@EntityScan("yalms.libraryapi.entities")
 @EnableTransactionManagement
 public class YalmsLibraryApplication {
     public static void main(String[] args) {
